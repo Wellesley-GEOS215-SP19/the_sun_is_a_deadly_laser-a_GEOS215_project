ececb2238ac542e4c0222ba41242677d9139ce8f
Leafia <lsherade@wellesley.edu>
2019-03-20 16:06:19 -0400

    added albedo, trying to read the geotiff files

--- thesunisadeadlylaser_a_GEOS215_project.m
+++ thesunisadeadlylaser_a_GEOS215_project.m
@@ -3,4 +3,24 @@
 
 
 %% Read in geotiff files from Global Solar Atlas
-GHI_file = geotiffread('GHI.tif')
+[GHI] = geotiffread('GHI.tif');
+TEMP_file = geotiffread('TEMP.tif');
+
+figure
+mapshow(GHI);
+
+%% Read in temp data from GISS data
+
+GISS_temp = readtable('GISS_temp.csv');
+
+%% Read in albedo
+% X, Y, T, albedo
+% X = longitude in degrees east
+% Y = latitude in degrees north
+% T = time in months since 1960-01-01
+% albedo = in percent, missing values are replaced with 999.99
+
+lat_albedo = ncread('albedo.nc','Y');
+lon_albedo = ncread('albedo.nc','X');
+time_albedo = ncread('albedo.nc','T');
+albedo = ncread('albedo.nc','albedo');
